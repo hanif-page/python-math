df068b2ba1e8ea32ee3f18e30bd987923433e32f
hanif-sudoapt <amrhanif15@gmail.com>
2020-12-14 10:06:03 +0700

    menghapus import fractions, mengubah string menjadi formatted string, menghapus spasi di beberapa awal string

--- pythagoras.py
+++ pythagoras.py
@@ -1,101 +1,100 @@
 #by M.A Hanif
-import fractions
+# menghapus import fractions
 import math
 
 def menu():
-    choice = input('\n Kamu mau mencari apa ? \n 1. Hipotenusa segitiga \n 2. Tinggi segitiga \n 3. Alas segitiga \n 4. Luas Segitiga \n 5. Jenis Segitiga \n 6. Tripel pythagoras \n 7.Exit \n ')
-    if choice == '1':
+    choice = int(input('\n Kamu mau mencari apa ? \n 1. Hipotenusa segitiga \n 2. Tinggi segitiga \n 3. Alas segitiga \n 4. Luas Segitiga \n 5. Jenis Segitiga \n 6. Tripel pythagoras \n 7. Exit \n '))
+    if choice == 1:
         hipotenusa()
-    elif choice == '2':
+    elif choice == 2:
         tinggi()
-    elif choice == '3':
+    elif choice == 3:
         alas()
-    elif choice == '4':
+    elif choice == 4:
         luas()
-    elif choice == '5':
+    elif choice == 5:
         tipe_segitiga()
-    elif choice == '6':
+    elif choice == 6:
         tripel()
     else:
         exit_out()
 
 def hipotenusa():
      print()
-     alas_segitiga = int(input(' Masukan nilai alas dari segitiga: '))
+     alas_segitiga = int(input('Masukan nilai alas dari segitiga: '))
      print()
-     tinggi_segitiga = int(input(' Masukan nilai tinggi dari segitiga: '))
+     tinggi_segitiga = int(input('Masukan nilai tinggi dari segitiga: '))
      print()
-     print( ' Sisi miring atau hipotenusa =' ,math.sqrt( alas_segitiga ** 2 + tinggi_segitiga ** 2) )
+     print(f'Sisi miring atau hipotenusa = {round(math.sqrt( alas_segitiga ** 2 + tinggi_segitiga ** 2))}') #di baris ini, gw tambahin round, buat iseng aja sbnrnya klo emg hasilnya koma wkwk
      menu()
 
 def tinggi():
     print()
-    hipotenusa_segitiga = int(input(' Masukan nilai hipotenusa dari segitiga: '))
+    hipotenusa_segitiga = int(input('Masukan nilai hipotenusa dari segitiga: '))
     print()
-    alas_segitiga = int(input(' Masukan nilai alas dari segitiga: '))
+    alas_segitiga = int(input('Masukan nilai alas dari segitiga: '))
     print()
-    print(  ' tinggi =', math.sqrt( hipotenusa_segitiga ** 2 - alas_segitiga ** 2) )
+    print(f'tinggi = {math.sqrt( hipotenusa_segitiga ** 2 - alas_segitiga ** 2)}')
     menu()
 
 def alas():
     print()
-    hipotenusa_segitiga = int(input(' Masukan nilai hipotenusa dari segitiga: '))
+    hipotenusa_segitiga = int(input('Masukan nilai hipotenusa dari segitiga: '))
     print()
-    tinggi_segitiga = int(input(' Masukan nilai tinggi dari segitiga: '))
+    tinggi_segitiga = int(input('Masukan nilai tinggi dari segitiga: '))
     print()
-    print( ' alas =', math.sqrt( hipotenusa_segitiga ** 2 - tinggi_segitiga ** 2) )
+    print(f'alas = {math.sqrt( hipotenusa_segitiga ** 2 - tinggi_segitiga ** 2)}')
     menu()
 
 def luas():
     print()
-    alas_segitiga = int(input(' Masukan nilai alas dari segitiga: '))
+    alas_segitiga = int(input('Masukan nilai alas dari segitiga: '))
     print()
-    tinggi_segitiga = int(input(' Masukan nilai tinggi dari segitiga: '))
+    tinggi_segitiga = int(input('Masukan nilai tinggi dari segitiga: '))
     print()
-    print( ' luas Segitiga =' ,alas_segitiga * tinggi_segitiga / 2)
+    print(f'luas Segitiga = {alas_segitiga * tinggi_segitiga / 2}')
     menu()
 
 def tipe_segitiga():
     print()
-    hipotenusa_segitiga = int(input(' Masukan nilai sisi miring (nilai tertinggi) \n ' ))
+    hipotenusa_segitiga = int(input('Masukan nilai sisi miring (nilai tertinggi) \n '))
     print()
-    alas_segitiga = int(input(' Masukan nilai alas \n '))
+    alas_segitiga = int(input('Masukan nilai alas \n '))
     print()
-    tinggi_segitiga = int(input(' Masukan nilai tinggi \n '))
+    tinggi_segitiga = int(input('Masukan nilai tinggi \n '))
 
     if hipotenusa_segitiga ** 2 == alas_segitiga ** 2 + tinggi_segitiga ** 2:
       print()
-      print(' Segitiga ini adalah segitiga siku-siku ')
+      print(f'Segitiga ini adalah segitiga siku-siku ')
       menu()
 
     elif hipotenusa_segitiga ** 2 < alas_segitiga ** 2 + tinggi_segitiga ** 2:
        print()
-       print(' Segitiga ini adalah segitiga lancip ')
+       print(f'Segitiga ini adalah segitiga lancip ')
        menu()
 
     else:
        print()
-       print(' Segitiga ini adalah segitiga tumpul ')
+       print(f'Segitiga ini adalah segitiga tumpul ')
        menu()
 
 def tripel():
     print()
-    hipotenusa_segitiga = int(input(' Masukan nilai sisi miring (nilai tertinggi) \n '))
-    tinggi_segitiga = int(input(' Masukan nilai tinggi \n '))
-    alas_segitiga = int(input(' Masukan nilai alas \n '))
+    hipotenusa_segitiga = int(input('Masukan nilai sisi miring (nilai tertinggi) \n '))
+    tinggi_segitiga = int(input('Masukan nilai tinggi \n '))
+    alas_segitiga = int(input('Masukan nilai alas \n '))
 
     if hipotenusa_segitiga ** 2 == alas_segitiga ** 2 + tinggi_segitiga ** 2:
         print()
-        print(' Segitiga ini termasuk tripel pythagoras ')
+        print(f'Segitiga ini termasuk tripel pythagoras ')
         menu()
 
     else:
         print()
-        print(' Segitiga ini tidak termasuk tripel pythagoras ')
+        print(f'Segitiga ini tidak termasuk tripel pythagoras ')
         menu()
 
 def exit_out():
-    print('\n Terima Kasih \n')
-
+    print(f'\n Terima Kasih \n')
 
 menu()
